9d706d49980064e1121b9865e9780a9b135f2c00
EliasOrellana437 <eliasernestoorellanavasquez@gmail.com>
2026-04-21 14:50:16 -0600

    conexionn

--- conexion.php
+++ conexion.php
@@ -1,10 +1,11 @@
 <?php
-$host = "XampTeOdio";
+$host = "127.0.0.1";   // o "localhost"
+$port = "3307";        // puerto que usa tu conexión XampTeOdio
 $user = "root";
-$pass = ""; // Cambiar si tienes contraseña en tu servidor local
+$pass = "";            // si no tienes contraseña en XAMPP
 $db   = "parcial2";
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
+$conexion = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
